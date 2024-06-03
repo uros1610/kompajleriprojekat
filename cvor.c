d1122ad2bdf73ff0716ca036271edb98e490bacf
Uroš Milović <urkemilovic16@gmail.com>
2024-06-04 00:17:00 +0200

    mala izmjena u cvoru

--- cvor.c
+++ cvor.c
@@ -3,28 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Cvor* kreirajCvor(char* vrijednost) {
-Cvor* noviCvor = malloc(sizeof(struct Cvor));
+#include "cvor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-noviCvor->vrijednost = strdup(vrijednost);
-noviCvor->broj_sinova = 0;
-noviCvor->sinovi = 0;
-noviCvor->sljedeci = 0;
-noviCvor->nivo = 0;
+Cvor* kreirajCvor(char* vrijednost) {
+    Cvor* noviCvor = (Cvor*)malloc(sizeof(Cvor));
+    
 
-return noviCvor;
+    noviCvor->vrijednost = strdup(vrijednost);
+    noviCvor->broj_sinova = 0;
+    noviCvor->sinovi = NULL;
+    noviCvor->sljedeci = NULL;
+    noviCvor->nivo = 0;
 
-
+    return noviCvor;
 }
 
-void dodajSina(Cvor* roditelj,Cvor* sin) {
+
+void dodajSina(Cvor* roditelj, Cvor* sin) {
     roditelj->broj_sinova++;
-    Cvor* noviCvor = malloc(sizeof(struct Cvor));
-    noviCvor->vrijednost = strdup(sin->vrijednost);
-    noviCvor->nivo = sin->nivo;
-    noviCvor->sljedeci = sin->sljedeci;
-    noviCvor->sinovi = sin->sinovi;
-    noviCvor->broj_sinova = sin->broj_sinova; 
-    roditelj->sinovi = realloc(roditelj->sinovi,(sizeof(Cvor)*roditelj->broj_sinova));
-    roditelj->sinovi[roditelj->broj_sinova-1] = noviCvor;
+    
+    roditelj->sinovi = (Cvor**)realloc(roditelj->sinovi, roditelj->broj_sinova * sizeof(Cvor*));
+  
+    
+    roditelj->sinovi[roditelj->broj_sinova - 1] = sin;
 }
